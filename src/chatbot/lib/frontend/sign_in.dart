a67b8a6f00894533cc199f8037e1402c7a5387d5
Lưu Thành Đạt <153430310+CrazyToff3@users.noreply.github.com>
2024-12-15 14:38:20 +0700

    Added UI to SignInPage

--- src/chatbot/lib/frontend/sign_in.dart
+++ src/chatbot/lib/frontend/sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../backend/services/auth_service.dart';
 import '../backend/config/logger.dart';
 
@@ -47,57 +48,154 @@ class _SignInPageState extends State<SignInPage> {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(title: const Text("Đăng nhập tài khoản")),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(labelText: "Email"),
+      body: SingleChildScrollView(
+        child: Container(
+          height: screenHeight,
+          width: screenWidth,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.purple,
+                Colors.pink,
+                Colors.red,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText: "Mật khẩu"),
-              obscureText: true,
-            ),
-            if (_errorMessage != null)
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Text(
-                  _errorMessage!,
-                  style: const TextStyle(color: Colors.red),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 60),
+              SizedBox(
+                child: Image.asset('images/logo.png', fit: BoxFit.contain),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Welcome to ChatGPT',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white,
                 ),
               ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _signIn,
-              
-              child: const Text("Đăng nhập"),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/signup');
-              },
-              child: const Text("Chưa có tài khoản? Đăng ký ngay."),
-            ),
-            TextButton(
-              onPressed: () async {
-                // Gửi email khôi phục mật khẩu
-                String email = _emailController.text.trim();
-                if (email.isEmpty) {
-                  setState(() {
-                    _errorMessage = "Email không thể để trống.";
-                  });
-                  return;
-                }
-                await _authService.sendPasswordResetEmail(email);
-              },
-              child: const Text("Quên mật khẩu?"),
-            ),
-          ],
+              const SizedBox(height: 20),
+              Container(
+                width: screenWidth * 0.6,
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Sign In',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.purple,
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    TextField(
+                      controller: _emailController,
+                      decoration: const InputDecoration(
+                        suffixIcon: Icon(
+                          FontAwesomeIcons.envelope,
+                          size: 17,
+                        ),
+                        hintText: 'Email',
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    TextField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        suffixIcon: Icon(
+                          FontAwesomeIcons.lock,
+                          size: 17,
+                        ),
+                        hintText: 'Password',
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                    if (_errorMessage != null)
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: Text(
+                          _errorMessage!,
+                          style: const TextStyle(color: Colors.red),
+                        ),
+                      ),
+                    TextButton(
+                      onPressed: () async {
+                        String email = _emailController.text.trim();
+                        if (email.isEmpty) {
+                          setState(() {
+                            _errorMessage = "Email cannot be empty.";
+                          });
+                          return;
+                        }
+                        await _authService.sendPasswordResetEmail(email);
+                      },
+                      child: const Text(
+                        'Forgot Password?',
+                        style: TextStyle(color: Colors.purple),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    GestureDetector(
+                      onTap: _signIn,
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          gradient: const LinearGradient(
+                            colors: [
+                              Colors.purple,
+                              Colors.pink,
+                              Colors.red,
+                            ],
+                            begin: Alignment.centerLeft,
+                            end: Alignment.centerRight,
+                          ),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, '/signup');
+                      },
+                      child: const Text("Don't have an account? Sign up."),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
