b7700c1d56a47645b780b17f42f9b2898b3c626c
Lưu Thành Đạt <153430310+CrazyToff3@users.noreply.github.com>
2024-12-19 17:40:30 +0700

    Add a forgot password page, add success message for reset password.

--- src/chatbot/lib/frontend/forgot_password.dart
+++ src/chatbot/lib/frontend/forgot_password.dart
@@ -1,29 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../backend/services/auth_service.dart';
-import '../backend/config/logger.dart';
-import 'forgot_password.dart';
 
-class SignInPage extends StatefulWidget {
-  const SignInPage({super.key});
+class ForgotPasswordPage extends StatefulWidget {
+  const ForgotPasswordPage({super.key});
 
   @override
-  _SignInPageState createState() => _SignInPageState();
+  _ForgotPasswordPageState createState() => _ForgotPasswordPageState();
 }
 
-class _SignInPageState extends State<SignInPage> {
+class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
   final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
   final AuthService _authService = AuthService();
-
-  // Track the visibility of the password
-  bool _isPasswordVisible = false;
 
   // Track the loading state to prevent multiple requests
   bool _isLoading = false;
 
-  // Sign-in method
-  void _signIn() async {
+  // Method to send password reset email
+  void _sendPasswordResetEmail() async {
     // Prevent multiple button presses
     if (_isLoading) return;
 
@@ -32,36 +26,19 @@ class _SignInPageState extends State<SignInPage> {
     });
 
     String email = _emailController.text.trim();
-    String password = _passwordController.text.trim();
-
-    // Check for specific errors
-    if (email.isEmpty && password.isEmpty) {
-      _showErrorMessage("Email and password cannot be empty.");
-      MyLogger.d("Email and password cannot be empty.");
-      setState(() => _isLoading = false); // Reset loading state
-      return;
-    }
 
     if (email.isEmpty) {
       _showErrorMessage("Email cannot be empty.");
-      MyLogger.d("Email cannot be empty.");
-      setState(() => _isLoading = false);
-      return;
-    }
-
-    if (password.isEmpty) {
-      _showErrorMessage("Password cannot be empty.");
-      MyLogger.d("Password cannot be empty.");
       setState(() => _isLoading = false);
       return;
     }
 
     try {
-      await _authService.signInWithEmail(email, password);
-      Navigator.pushReplacementNamed(context, '/chat');
+      await _authService.sendPasswordResetEmail(email);
+      _showSuccessMessage(
+          "An email has been sent to the email address. Check the inbox of the provided email.");
     } catch (e) {
       _showErrorMessage(e.toString());
-      MyLogger.d("Error during sign-in: $e");
     } finally {
       setState(() {
         _isLoading = false; // Reset loading state after request
@@ -69,14 +46,7 @@ class _SignInPageState extends State<SignInPage> {
     }
   }
 
-  // Toggle password visibility
-  void _togglePasswordVisibility() {
-    setState(() {
-      _isPasswordVisible = !_isPasswordVisible;
-    });
-  }
-
-    void _showSuccessMessage(String message) {
+  void _showSuccessMessage(String message) {
     showDialog(
       context: context,
       barrierDismissible: true,
@@ -108,6 +78,7 @@ class _SignInPageState extends State<SignInPage> {
                     ),
                     onPressed: () {
                       Navigator.of(context).pop();
+                      Navigator.pushReplacementNamed(context, '/signin');
                     },
                   ),
                 ],
@@ -181,7 +152,7 @@ class _SignInPageState extends State<SignInPage> {
       body: SingleChildScrollView(
         child: ConstrainedBox(
           constraints: BoxConstraints(
-            minHeight: screenHeight > 600 ? screenHeight : 600, // Set a minimum height
+            minHeight: screenHeight > 600 ? screenHeight : 600,
           ),
           child: Container(
             width: screenWidth,
@@ -205,7 +176,7 @@ class _SignInPageState extends State<SignInPage> {
                 ),
                 const SizedBox(height: 20),
                 const Text(
-                  'Welcome to ChatGPT',
+                  'Forgot Password',
                   style: TextStyle(
                     fontSize: 22,
                     color: Colors.white,
@@ -213,9 +184,9 @@ class _SignInPageState extends State<SignInPage> {
                 ),
                 const SizedBox(height: 20),
                 Container(
-                  width: screenWidth * 0.3,  // Adjust this width to be dynamic
+                  width: screenWidth * 0.3,
                   constraints: const BoxConstraints(
-                    minWidth: 300, // Minimum width for the login box
+                    minWidth: 300,
                   ),
                   padding: const EdgeInsets.all(20),
                   decoration: BoxDecoration(
@@ -225,7 +196,7 @@ class _SignInPageState extends State<SignInPage> {
                   child: Column(
                     children: [
                       const Text(
-                        'Sign In',
+                        'Reset Your Password',
                         style: TextStyle(
                           fontSize: 24,
                           fontWeight: FontWeight.bold,
@@ -246,60 +217,20 @@ class _SignInPageState extends State<SignInPage> {
                           ),
                         ),
                       ),
-                      const SizedBox(height: 10),
-                      TextField(
-                        controller: _passwordController,
-                        obscureText: !_isPasswordVisible,
-                        decoration: InputDecoration(
-                          suffixIcon: IconButton(
-                            icon: Icon(
-                              _isPasswordVisible
-                                  ? FontAwesomeIcons.eye
-                                  : FontAwesomeIcons.eyeSlash,
-                              size: 17,
-                              color: Colors.grey,
-                            ),
-                            onPressed: _togglePasswordVisibility,
-                          ),
-                          hintText: 'Password',
-                          hintStyle: const TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 15),
-                      TextButton(
-                        onPressed: () async {
-                          Navigator.pushNamed(context, '/forgot-password');
-                        },
-                        child: const Text(
-                          'Forgot Password?',
-                          style: TextStyle(color: Colors.purple),
-                        ),
-                      ),
-                      const SizedBox(height: 15),
+                      const SizedBox(height: 20),
                       ElevatedButton(
-                        onPressed: _isLoading ? null : _signIn, // Disable button when loading
+                        onPressed:
+                            _isLoading ? null : _sendPasswordResetEmail, // Disable button when loading
                         style: ElevatedButton.styleFrom(
                           padding: const EdgeInsets.all(15),
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(50),
                           ),
-                          elevation: 0, // No shadow
-                          backgroundColor: Colors.transparent, // Allows the gradient to show
+                          elevation: 0,
+                          backgroundColor: Colors.transparent,
                         ).copyWith(
-                          foregroundColor: WidgetStateProperty.all(Colors.white),
-                          overlayColor: WidgetStateProperty.resolveWith<Color?>(
-                            (states) {
-                              if (states.contains(WidgetState.pressed)) {
-                                return Colors.pinkAccent.withAlpha(50); // Ripple effect when pressed
-                              }
-                              if (states.contains(WidgetState.hovered)) {
-                                return Colors.pinkAccent.withAlpha(25); // Hover effect
-                              }
-                              return null; // Default
-                            },
-                          ),
+                          foregroundColor:
+                              WidgetStateProperty.all(Colors.white),
                         ),
                         child: Ink(
                           decoration: BoxDecoration(
@@ -312,9 +243,10 @@ class _SignInPageState extends State<SignInPage> {
                           ),
                           child: Center(
                             child: _isLoading
-                                ? const CircularProgressIndicator(color: Colors.white)
+                                ? const CircularProgressIndicator(
+                                    color: Colors.white)
                                 : const Text(
-                                    'Login',
+                                    'Send Reset Email',
                                     style: TextStyle(
                                       fontSize: 20,
                                       fontWeight: FontWeight.bold,
@@ -326,9 +258,9 @@ class _SignInPageState extends State<SignInPage> {
                       const SizedBox(height: 15),
                       TextButton(
                         onPressed: () {
-                          Navigator.pushReplacementNamed(context, '/signup');
+                          Navigator.pushReplacementNamed(context, '/signin');
                         },
-                        child: const Text("Don't have an account? Sign up."),
+                        child: const Text("Back to Sign Up"),
                       ),
                     ],
                   ),
